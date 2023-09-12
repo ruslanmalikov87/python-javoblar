5d64d41e75c0ba8a386478ac448772cb3da4545f
ruslanmalikov87 <144749246+ruslanmalikov87@users.noreply.github.com>
2023-09-12 19:20:13 +0500

    yangi fayl
    2 dars fayli qo'shildi

---
+++ 02-dars_amaliyot.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 23 08:00:34 2023
+
+@author: user-pc
+"""
+
+print("Assalomu alaykum")
+print('Xayirli tong')
+print(2+4*2)
+print(19/5)
+print(2**4)
